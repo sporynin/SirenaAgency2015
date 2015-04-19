ade48f5b2a6d0a3903153736f082ea0b2893cb5a
Yuriy Sporynin <yuran@egopay.ru>
2015-04-19 23:43:19 +0300

    fix errors

--- js/conversion.js
+++ js/conversion.js
@@ -1,42 +1,12 @@
-/**
- * Created by root on 4/18/15.
- */
-function dorate(id, data, col) {
-    var width = 960, height = 500;
-
-    var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-    var y = d3.scale.linear().range([height, 0]);
-    var color = d3.scale.ordinal().range(col);
-    color.domain(["tch", "bsp"]);
-    var chart = d3.select(id).attr("width", width).attr("height", height);
-
-    data.forEach(function(d) {
-       d.bars = [{name : "tch", y0 : 0, y1 : d.tch}, {name : "bsp", y0 : d.tch, y1 : d.tch + d.bsp}];
-       d.total = d.tch + d.bsp;
-    });
-    y.domain([0, d3.max(data, function(d) { return d.total; })]);
-    x.domain(data.map(function(d) { return parseDate(d.date); }));
-    var bar = chart.selectAll("g")
-        .data(data).enter().append("g")
-        .attr("transform", function(d) { return "translate(" + x(parseDate(d.date)) + ", 0)"; });
-
-    bar.selectAll("rect")
-        .data(function(d) { return d.bars; }).enter().append("rect")
-        .attr("y", function(d) { return y(d.y1); })
-        .attr("height", function(d) { return y(d.y0) - y(d.y1); })
-        .attr("width", x.rangeBand())
-        .style("fill", function(d) { return color(d.name); });
-    /*
-    bar.append("text")
-        .attr("x", barWidth / 2)
-        .attr("y", function(d) { return y(d.close) + 3; })
-        .attr("dy", ".5em")
-        .text(function(d) { return d.close; });
-    */
-
-};
-
 window.addEventListener("load", function() {
-     dorate("#ratsumchart", sum, ["blue", "red"]);
-     dorate("#ratcntchart", cnt, ["green", "orange"]); 
+    var cons = liquidFillGaugeDefaultSettings();
+    cons.waveAnimateTime = 10000;
+    var risks = liquidFillGaugeDefaultSettings();
+    risks.waveAnimateTime = 10000;
+    risks.circleColor = 'red';
+    risks.waveColor = 'red';
+    loadLiquidFillGauge("conversion1", 95, cons);
+    loadLiquidFillGauge("risk1", 95, risks);
+    loadLiquidFillGauge("conversion2", 75, cons);
+    loadLiquidFillGauge("risk2", 0, risks);
 });
--- js/slides.js
+++ js/slides.js
@@ -1,6 +1,7 @@
 require(['order!../slide_config', 'order!modernizr.custom.45394',
          'order!prettify/prettify', 'order!hammer', 'order!slide-controller',
-         'order!slide-deck','order!d3.min', 'order!common', 'order!rates', 'order!browser', 
-         'order!speed', 'order!grow'], function(someModule) {
+         'order!slide-deck','order!d3.min', 'order!liquidbase',
+         'order!common', 'order!rates', 'order!browser', 
+         'order!speed', 'order!grow', 'order!conversion'], function(someModule) {
 
 });
